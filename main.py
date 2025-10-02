575b2528fa5e56b5340d66076ea037c7d7e3486c
pro99d <pro999999d@gmail.com>
2025-10-02 16:21:56 +0300

    изменено:      example.mylang 	изменено:      main.py

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
+globals = {}
+SPEC_CHR = [" ", "=", "\n", ";", "==", ">", "<", "<=", ">=", "+", "-", "/", "*"]
+
+class variable:
+    def __init__(self, name, _type, value):
+        self.name = name
+        self.type = _type
+        self.value = value
+
+def is_string(s: str):
+    symb = s[0]
+    if s[0] != '"' and s[-1] != '"':
+        return False
+    return s.count('"')==2
+     
+
+def parse_line(line: str):
+    cur_string = ''
+    result = []
+
+    for i in line:
+        if i not in SPEC_CHR:
+            cur_string+=i 
+            continue
+        elif i == " ":
+            result.append(cur_string)
+            cur_string = ""
+            continue
+        elif i == "=":
+            result.append(cur_string)
+            cur_string = ""
+            result.append('=')
+            continue
+    result.append(cur_string)
+    for d, i in enumerate(result):
+        if not i:
+            result.remove(i)
+        if i.isnumeric():
+            result[d] = int(i)
+    print(result)
 def main():
-    print("Hello from mylang!")
+    f = open("./example.mylang", 'r').readlines()
+    for l in f:
+        parse_line(l)
 
 
 if __name__ == "__main__":
