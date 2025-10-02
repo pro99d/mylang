08e4fd6dbd273a5d0a186a6261f595593cb5762a
pro99d <pro999999d@gmail.com>
2025-10-02 18:27:47 +0300

    добавленна поддержка переменных

---
+++ tests/strings.py
@@ -0,0 +1,17 @@
+import unittest
+import main
+
+class TestStringMethods(unittest.TestCase):
+
+    def test(self):
+        strings = [
+            ('"this is string"', True),
+            ('"open quote', False),
+            ('close quote"', False),
+            ('"12341"', True),
+        ]
+        for test_case in strings:
+            self.assertEqual(main.is_string(test_case[0]), test_case[1])
+
+if __name__ == '__main__':
+    unittest.main()
