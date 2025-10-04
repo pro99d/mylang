19dc92d64fad0b0e5d44c14857207c479c4378ad
pro99d <pro999999d@gmail.com>
2025-10-04 08:09:29 +0300

    добавлена поддержка дробей

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ globals = {}
 SPEC_CHR = [" ", "=", "\n", ";", "==", ">",
             "<", "<=", ">=", "+", "-", "/", "*"]
 # паттерны
-set_pattern = r'^[^=]+=[^=]+$'
+
+
+class Patterns:
+    set_pattern = r'^[^=]+=[^=]+$'
+    float_pattern = r'^[\d^.]+.[\d^.]+$'
 
 
 class Variable:
@@ -11,10 +15,54 @@ class Variable:
         self.name = name
         self.type = _type
         self.value = value
+
     def __repr__(self) -> str:
         return str(self.value)
 
+    # арифметические операции
+    def __add__(self, other):
+        if type(other) is self:
+            return self.value+other.value
+        return self.value+other
+
+    def __radd__(self, other):
+        if type(other) is self:
+            return self.value+other.value
+        return self.value+other
+    
+
+    def __div__(self, other):
+        if type(other) is self:
+            return self.value/other.value
+        return self.value/other
+
+    def __rdiv__(self, other):
+        if type(other) is self:
+            return other.value/self.value
+        return other/self.value
+
+    def __mul__(self, other):
+        if type(other) is self:
+            return self.value*other.value
+        return self.value*other
+
+    def __rmul__(self, other):
+        if type(other) is self:
+            return self.value*other.value
+        return self.value*other
+
+class Function:
+    def __init__(self, values):
+        self.values = values
+
+    def __call__(self, *args, **kargs):
+        pass
+
+
 variables = {}  # имя: Variable
+"""
+ 
+"""
 
 
 class Expression:
@@ -29,21 +77,26 @@ class Expression:
                 e = self.expr.split("=")
                 set_var = e[0]
                 set_value = e[1]
-                if is_string(set_value): #FIXME тип и значение c - строка
+                if is_string(set_value):
                     set_type = str
                     set_value = set_value[1:-1]
-                elif set_value.isnumeric:
+                elif set_value.isnumeric and '.' not in set_value:
                     set_type = int
+                elif re.fullmatch(Patterns.float_pattern, set_value):
+                    set_type = float
                 else:
                     set_type = type(set_value)
 
-                variables[set_var] = Variable(set_var, set_type, set_value)
+                variables[set_var] = Variable(
+                    set_var, set_type, set_type(set_value))
+            case "add":
+                pass # TODO result[::-1] ?
             case _:
                 raise Exception(
                     f"Unimplemented expression type: {self.expr_type}")
 
     def __repr__(self) -> str:
-        return self.expr[1:-1]
+        return self.expr  # [1:-1]
 
 
 def is_string(s: str):
@@ -59,18 +112,7 @@ def delete_empty(wtp: list) -> list:
     return wtp
 
 
-def devide_to_lines(r):
-    result = [""]
-    for i in r:
-        if i != ";":
-            result[-1] += str(i)
-        else:
-            result.append("")
-    result = delete_empty(result)
-    return result
-
-
-def parse_line(line: str):
+def devide_by_spec(line: str):
     cur_string = ''
     result = []
     for i in line:
@@ -89,21 +131,52 @@ def parse_line(line: str):
             result[d] = int(i)
     while result.count("\n"):
         result.remove('\n')
+    return result
 
+
+def devide_to_lines(r):
+    """
+    TODO:
+    скопипастить разделение на токеты в функцию
+    использовать тут
+    """
+    result = [""]
+    r = iter(r)
+    while True:
+        try:
+            try:
+                if i:
+                    pass
+            except NameError:
+                pass
+            i = next(r)
+            if i != ";":
+                result[-1] += str(i)
+            else:
+                result.append("")
+        except StopIteration:
+            break
+    result = delete_empty(result)
+    return result
+
+
+def parse_line(line: str):
+    result = devide_by_spec(line)
     result = devide_to_lines(result)
 
     for d, i in enumerate(result):
-        if re.fullmatch(set_pattern, i):
+        if re.fullmatch(Patterns.set_pattern, i):
             result[d] = Expression(i, "set")
         else:
             result[d] = Expression(i, "undefined")
 
-    
     return result
+
 
 def execute_expressions(expressions: list[Expression]) -> None:
     for i in expressions:
         i.find_result()
+
 
 def main():
     f = "".join(open("./example.mylang", 'r').readlines())
